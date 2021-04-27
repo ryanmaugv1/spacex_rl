34d40c25680729d6ca488825321b745203a20ffb
Ryan Maugin <ryanmaugv1@gmail.com>
2021-04-27 10:43:25 +0100

    Added empty helper methods requirning implementation for agent.

--- Assets/Scripts/SN11Agent.cs
+++ Assets/Scripts/SN11Agent.cs
@@ -22,7 +22,7 @@ using Unity.MLAgents.Actuators;
 public class SN11Agent : Agent
 {
     /// Agent thruster transform used for applying force at position for rocket.
-    public Transform AgentThruster;
+    public Transform AgentThrustVector;
     /// Landing pad transform used for relative positioning of rocket and reward calculation.
     public Transform LandingPad;
 
@@ -37,53 +37,116 @@ public class SN11Agent : Agent
 
 
     /**
-     * Initializing & Resetting Agent On Episode Begin
+     *  Initializing & Resetting Agent On Episode Begin
      *
-     * We will initialise/reset our agent in the followng state:
-     *      1) Set Y-axis transform position within 250-500CM range relative to landing pad.
+     *  We will initialise/reset our agent in the followng state:
+     *      1) Set Y-axis transform position within 250-500CM range.
      *      2) Set X & Z axis transform position within 50-100CM radius relative to landing pad.
      *      3) Set X, Y & Z axis transform rotation randomly.
      *
-     * We want to reset our agent if we end up in the following states:
-     *      1) Agent Lands Upright (and doesn't move for 5 second)
-     *      2) Agent Crashes
-     *      3) Agent Falls Below Landing Pad
+     *  This should ensure reset/init the environment in random acceptable states
+     *  so that our agent can learn a more general and robust policy.
      */
     public override void OnEpisodeBegin() {}
+
+
+    #region OnEpisodeBegin Helper Methods
+
+
+    /// Set agent Y position randomly within height range.
+    private void SetAgentYPosition() {}
+
+
+    /// Set agent X & Z position randomly within area range relative to landing pad.
+    private void SetAgentXZPosition() {}
+
+
+    /// Set agent orientation randomly.
+    private void SetRandomAgentOrientation() {}
+
+
+    #endregion
 
 
     /**
      *  Collect Environment & Agent Sensor Observations
      *
      *  The observations we gather from the environment and agent are:
-     *      1) Agent relative position to landing pad (x, y, z).
-     *      2) Agent relative rotation to landing pad (x, y, z).
-     *      3) Agent velocity (x, y, z).
-     *      4) Agent distance to the landing pad (y in metres).
-     *      5) Agent angular momentum (x, y, z).
-     *      6) Agent thrust vector rotation (x, z).
-     *      7) Agent thrust force being applied (Kilo Newton).
+     *      - Agent relative position to landing pad (x, y, z).
+     *      - Agent orientation (x, y, z).
+     *      - Agent velocity (x, y, z).
+     *      - Agent distance to the landing pad/ground (y in metres).
+     *      - Agent angular momentum (x, y, z).
+     *      - Agent thrust vector orientation (x, z).
+     *      - Agent thrust force being applied (Newton).
      *
      *  We collect a total of 16 observations to train our agent with.
      */
     public override void CollectObservations(VectorSensor sensor) {}
+
+    #region CollectObservations Helper Methods
+
+
+    /// Returns agent relative position from landing pad (x, y, z respectively)
+    private Vector3 GetAgentRelativePositionFromLandingPad() {
+        return Vector3.zero;
+    }
+
+
+    /// Returns agent orientation (x, y, z respectively).
+    private Vector3 GetAgentOrientation() {
+        return Vector3.zero;
+    }
+
+
+    /// Returns agent velocity (x, y, z respectively).
+    private Vector3 GetAgentVelocity() {
+        return Vector3.zero;
+    }
+
+
+    /// Returns agent distance from ground in metres.
+    private float GetAgentDistanceFromGround() {
+        return 0.0f;
+    }
+
+
+    /// Returns agent angular momentum (x, y, z respectively).
+    private Vector3 GetAgentAngularMomentum() {
+        return Vector3.zero;
+    }
+
+
+    /// Returns agent thrust vector orientation (x, z respectively).
+    private (float x, float z) GetThrustVectorOrientation() {
+        return (0.0f, 0.0f);
+    }
+
+    
+    /// Returns agent current thrust force in Newton (N).
+    private float GetAgentCurrentThrustForce() {
+        return 0.0f;
+    }
+
+
+    #endregion
 
 
     /**
      *  Performing Action & Rewarding Agent
      *
      *  Our agent action are as follows:
-     *      1) Rotating thrust vector (x, z).
-     *      2) Thrust force output (newton).
+     *      - Rotating thrust vector (x, z).
+     *      - Thrust force output (newton).
      *
      *  The agent rewarding logic looks like this:
-     *      1) Reward in range of 0 to 1 where:
+     *      - Reward in range of 0 to 1 where:
      *          0 = Landed just outside edge of landing pad.
      *          1 = Landed dead centre of landing pad.
-     *      2) Reward in range of 0 to 0.1 where:
+     *      - Reward in range of 0 to 0.1 where:
      *          0 = Rocket orientation isn't within upright range.
      *          1 = Rocket orientation is within upright range.
-     *      3) Reward in range of 0 to 1 where:
+     *      - Reward in range of 0 to 1 where:
      *          0 = Touched ground at velocity greater than 10 m/s.
      *          1 = Touched ground at velocity less than 1 m/s.
      *          TODO: Find better MPH values that aren't so arbitrary.
@@ -91,6 +154,61 @@ public class SN11Agent : Agent
      *  Using the defined action an reward space above we should be able to
      *  find an optimal policy for self-landing the agent rocket upright on
      *  a designated landing pad.
+     *
+     *  We want to reset our agent if we end up in the following states:
+     *      - Agent Lands Upright (and doesn't move for 5 second)
+     *      - Agent Crashed
+     *      - Agent Out Of Range
      */
     public override void OnActionReceived(ActionBuffers actionBuffers) {}
+
+
+    #region OnActionReceived Helper Methods
+
+
+    #region State Check Helpers
+
+
+    /// Checks if agent has landed upright (we don't care about whether it's on pad or not).
+    private bool HasAgentLanded() {
+        return false;
+    }
+
+
+    /// Check if agent has landed on landing pad (we don't care about orientation).
+    private bool HasAgentLandedOnPad() {
+        return false;
+    }
+
+
+    /// Check if agent has crashed (not landed upright) on pad or not.
+    private bool HasAgentCrashed() {
+        return false;
+    }
+
+
+    /// Check if agent has flown too far out of range on any positional axis relative to landing pad.
+    private bool IsAgentOutOfRange() {
+        return false;
+    }
+
+
+    #endregion
+
+
+    #region Action Helpers
+
+
+    /// Set thrust vector orientation (x, z) based on agent predicted continuous actions.
+    private void SetThrusterOrientation(ActionBuffers actionBuffers) {}
+
+
+    /// Set thrust force within 0 to 12,000 Newton based on agent predicted continuous action.
+    private void SetThrustForce(ActionBuffers actionBuffers) {}
+
+
+    #endregion
+
+
+    #endregion
 }
